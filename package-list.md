b607affda3b39ef92b4fbf5b1eb0fabd49c6b173
qweru <itsbookx@gmail.com>
2023-11-04 19:48:22 +0000

    [+] Rofi, package list

---
+++ package-list.md
@@ -0,0 +1,12 @@
+## Package list
+- Alacritty
+- Eww (optional)
+- Hyprland
+- Hyprpaper
+- Kitty (optional)
+- Neofetch
+- Rofi
+- Swaylock
+- Waybar
+
+### Probably more but im lazy
---
+++ waybar/waybar/scripts/spotify.sh
@@ -0,0 +1,5 @@
+while true
+do
+    pgrep 'spotify' | echo {\"text\": \" $(playerctl metadata --format "{{ title }} - {{ artist }}")\", \"alt\": \"test alt\", \"tooltip\": \"test tooltip\", \"class\": \"test-class\", \"percentage\": \"69%\"}
+    sleep 0.4
+done
