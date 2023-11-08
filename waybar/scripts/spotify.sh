ade52d68dc48e15d366356a93198244d085fa7f6
qweru <itsbookx@gmail.com>
2023-11-08 14:28:06 +0000

    + auto-update

--- waybar/scripts/spotify.sh
+++ waybar/scripts/spotify.sh
@@ -1,5 +1,12 @@
 while true
 do
-    pgrep 'spotify' | echo {\"text\": \" $(playerctl metadata --format "{{ title }} - {{ artist }}")\", \"alt\": \"test alt\", \"tooltip\": \"test tooltip\", \"class\": \"test-class\", \"percentage\": \"69%\"}
+    # echo {\"text\": \"Nothing is playing right now!\", \"alt\": \"Player\", \"tooltip\": \"Player\", \"class\": \"player-display\", \"percentage\": \"1%\"}
+    # echo "$(playerctl metadata --format '{{ title }} - {{ artist }}')"
+    if [[ "$(playerctl metadata --format '{{ title }} - {{ artist }}')" != "" ]] 
+    then
+        echo "{\"text\": \"$(playerctl metadata --format "{{ title }} - {{ artist }}")\", \"alt\": \"Player\", \"tooltip\": \"Player\", \"class\": \"player-display\", \"percentage\": \"1%\"}"
+    else
+        echo "{\"text\": \"Nothing is playing right now!\", \"alt\": \"Player\", \"tooltip\": \"Player\", \"class\": \"player-display\", \"percentage\": \"1%\"}"
+    fi
     sleep 0.4
 done
